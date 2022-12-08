9c9ed2fd9f28ee9d327ddadebd90f88ae1fa20e3
Shashi <shashikumarsingh686@gmail.com>
2022-12-08 11:59:06 +0530

    testing changes

--- Function/src/Avg_of3_number.java
+++ Function/src/Avg_of3_number.java
@@ -1,6 +1,6 @@
 public class Avg_of3_number {
     public static void main(String[] args) {
-        int a= 25,b=34,c=23;
+        int a= 25,b=34,c=29;
         avg(a,b,c);
     }
     public static  void avg(int a, int b, int c) {
